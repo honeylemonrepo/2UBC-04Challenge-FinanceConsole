388ec8831f7e8d16b6347e969fb74dbcd8d46a48
honeylemonrepo <hmi.csbootcamp@gmail.com>
2022-11-15 23:48:52 +0000

    initial

---
+++ my_file.js
@@ -0,0 +1,17 @@
+// variables functions
+var totalMonths = finances.length;
+// var totalProfit = finances.filter(pf);
+// function pf() {
+//     for (i=0; i < finances.length; i++) {
+//         let profit = [];
+//         return profit = finance[i++][i++] > 0;
+//     }
+// }
+
+// // operational statements
+// pf();
+
+// outcome
+console.log(totalMonths);
+// console.log(profit);
+// console.log(totalProfit);
